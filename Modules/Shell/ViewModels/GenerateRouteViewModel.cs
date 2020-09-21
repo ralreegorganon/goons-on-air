e1846a972d98eece872998e79e2afd941b92af97
Jason Jones <jasonedwardjones@gmail.com>
2020-09-21 09:03:08 -0800

    wip

---
+++ Modules/Shell/ViewModels/GenerateRouteViewModel.cs
@@ -0,0 +1,18 @@
+﻿using System.Diagnostics;
+using Caliburn.Micro;
+using Google.OrTools.LinearSolver;
+
+namespace GoonsOnAir.Modules.Shell.ViewModels
+{
+    public class GenerateRouteViewModel : Screen
+    {
+        public GenerateRouteViewModel()
+        {
+            DisplayName = "Generate Route";
+        }
+
+        public void GenerateRoute()
+        {
+        }
+    }
+}
--- Modules/Shell/ViewModels/ShellViewModel.cs
+++ Modules/Shell/ViewModels/ShellViewModel.cs
@@ -22,6 +22,7 @@ namespace GoonsOnAir.Modules.Shell.ViewModels
         public RefreshFboQueriesViewModel RefreshFboQueriesViewModel { get; set; }
         public AutoAcceptViewModel AutoAcceptViewModel { get; set; }
         public AddFavoriteMissionViewModel AddFavoriteMissionViewModel { get; set; }
+        public GenerateRouteViewModel GenerateRouteViewModel { get; set; }
 
         protected override async Task OnActivateAsync(CancellationToken cancellationToken)
         {
@@ -57,6 +58,11 @@ namespace GoonsOnAir.Modules.Shell.ViewModels
         public async Task SwitchToAddFavoriteMission()
         {
             await ActivateItemAsync(AddFavoriteMissionViewModel, CancellationToken.None);
+        }
+
+        public async Task SwitchToGenerateRoute()
+        {
+            await ActivateItemAsync(GenerateRouteViewModel, CancellationToken.None);
         }
 
         public Task HandleAsync(StatusMessageEvent message, CancellationToken cancellationToken)
